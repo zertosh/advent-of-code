922200af26678485832531adc74f03dbc9d54128
Andres Suarez <zertosh@gmail.com>
2021-12-07 16:21:31 -0500

    2021-7: clearer math functions

--- 2021/day_7.js
+++ 2021/day_7.js
@@ -8,25 +8,34 @@ const INPUT = require('fs')
   .split(',')
   .map(x => parseInt(x));
 
-INPUT.sort((a, b) => a - b);
+function calc_median_unsorted(values) {
+  const arr = values.slice().sort((a, b) => a - b);
+  const mid = Math.floor(arr.length / 2);
+  return arr.length % 2 !== 0 ? arr[mid] : (arr[mid - 1] + arr[mid]) / 2;
+}
+
+function calc_mean(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; sum += arr[i++]) {}
+  return sum / arr.length;
+}
 
 (function part_1() {
-  const median = INPUT[INPUT.length / 2];
+  const median = Math.floor(calc_median_unsorted(INPUT));
   let min_fuel = 0;
   for (const x of INPUT) {
-    min_fuel += Math.abs(median - x);
+    const dist = x < median ? median - x : x - median;
+    min_fuel += dist;
   }
   assert.equal(min_fuel, 359648);
 })();
 
 (function part_2() {
-  const average = Math.floor(
-    INPUT.reduce((acc, x) => (acc += x)) / INPUT.length,
-  );
+  const mean = Math.floor(calc_mean(INPUT));
   let min_fuel = 0;
   for (const x of INPUT) {
-    let dist = Math.abs(average - x);
-    let cost = (Math.pow(dist, 2) + dist) / 2; // https://en.wikipedia.org/wiki/Summation
+    const dist = x < mean ? mean - x : x - mean;
+    const cost = (dist * (dist + 1)) / 2; // https://en.wikipedia.org/wiki/Summation
     min_fuel += cost;
   }
   assert.equal(min_fuel, 100727924);
--- 2021/day_7.rs
+++ 2021/day_7.rs
@@ -1,17 +1,31 @@
 // https://adventofcode.com/2021/day/7
 
+fn calc_median_unsorted(values: &[usize]) -> usize {
+    let mut sorted = values.to_vec();
+    sorted.sort_unstable();
+
+    let mid = sorted.len() / 2;
+    if values.len() % 2 != 0 {
+        sorted[mid]
+    } else {
+        (sorted[mid - 1] + sorted[mid]) / 2
+    }
+}
+
+fn calc_mean(values: &[usize]) -> usize {
+    values.iter().sum::<usize>() / values.len()
+}
+
 fn main() {
-    let mut input: Vec<usize> = include_str!("day_7.txt")
+    let input: Vec<usize> = include_str!("day_7.txt")
         .trim_end_matches('\n')
         .split(',')
         .map(|x| x.parse::<usize>().unwrap())
         .collect();
 
-    input.sort();
-
     // Part 1
     {
-        let median = input[input.len() / 2];
+        let median = calc_median_unsorted(&input);
         let min_fuel = input
             .iter()
             .map(|x| if *x < median { median - x } else { x - median })
@@ -21,12 +35,12 @@ fn main() {
 
     // Part 2
     {
-        let avg = input.iter().sum::<usize>() / input.len();
+        let mean = calc_mean(&input);
         let min_fuel = input
             .iter()
-            .map(|x| if *x < avg { avg - x } else { x - avg })
             .map(|x| {
-                (x.pow(2) + x) / 2 // https://en.wikipedia.org/wiki/Summation
+                let dist = if *x < mean { mean - x } else { x - mean };
+                (dist * (dist + 1)) / 2 // https://en.wikipedia.org/wiki/Summation
             })
             .sum::<usize>();
         assert_eq!(min_fuel, 100727924);
